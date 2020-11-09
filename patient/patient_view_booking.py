3f96409f0c67291dcd7e42fa1bcb56b6aad700e9
fweni <fweni@student.wethinkcode.co.za>
2020-11-09 15:56:24 +0200

    Patient skeleton created.

---
+++ patient/patient_view_booking.py
@@ -0,0 +1,6 @@
+def view_booking():
+    """"
+    Patient will be able to see a list of all their bookings.
+    """
+    print("These are your bookings")
+    print(": booking with Lucky for Toy Robot 4 at 15h00.")
---
+++ patient/patient_view_open_booking.py
@@ -0,0 +1,9 @@
+def view_open_bookings():
+    """
+    The patient will be able to see a list of all available slots
+    """
+    print("""
+    9:00 hangman 1 - Luke
+    9:30 recursion - Florah
+    10:00 TDD(unittesting) - Sett
+    """)
