f7ed8813f0438ae5c24bbd9781a13cb92b87461f
moolivie <moolivie@student.wethinkcode.co.za>
2020-11-10 17:31:22 +0200

    test_view moved to main directory.

--- clinician/view_events.py
+++ clinician/view_events.py
@@ -1,13 +1,8 @@
 
 def view():
     '''This function displays the events that have been booked.'''
-<<<<<<< HEAD
+
     events_set=[('Recursion ' ,' Luke','11/11/2020' ,'14:30')]
     return str(events_set)
 
-view()
-=======
-    print("Booked events:")
-    print("Recursion -Luke")
-    print("List comprehension -Issa")
->>>>>>> main
+
