475b2f16f9098867209f565926bf2168d925055e
lmatsabu <lmatsabu@student.wethinkcode.co.za>
2020-11-11 11:53:11 +0200

    Startup function for google api

--- code_clinic.py
+++ code_clinic.py
@@ -4,11 +4,44 @@ from configparser import ConfigParser
 import getpass
 from patient import patient_cancels_booking,patient_make_booking,patient_view_booking,patient_view_open_booking
 from clinician import create, delete,view_events
-import sys
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
+def startup():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+    return service
 
 
 def run_clinic():
     '''Function to run Code Clinic and parse through commands from user'''
+
+    # service = startup()
 
     parser = argparse.ArgumentParser("Create and book slots for Code Clinics: -h or --help of list of options")
 
