061aa8ac5a634c190e223f275da5d4a72002f933
moolivieJHB <67946957+moolivieJHB@users.noreply.github.com>
2020-11-12 12:11:18 +0200

    Update test_view.py

--- test_view.py
+++ test_view.py
@@ -1,15 +1,29 @@
 import unittest
 from clinician import view_events
 from io import StringIO
+import sys
 
 class TestViewEvents(unittest.TestCase):
 
     def test_view(self):
         '''Test if function returns a set of booked events.'''
+        orig_stdout = sys.stdout
+        new_string = StringIO()
+        sys.stdout = new_string
         result = view_events.view()
         self.assertTrue(type(result), set)
+        sys.stdout = orig_stdout
 
     def test_view_result(self):
         '''Test if function resturn the actual list of booked events.'''
+        orig_stdout = sys.stdout
+        new_string = StringIO()
+        sys.stdout = new_string
         result=view_events.view()
-        self.assertEqual(result, "[('Recursion ', ' Luke', '11/11/2020', '14:30')]")
+        self.assertEqual(result, "['Recursion ', ' Luke', '11/11/2020', '14:30']")
+        sys.stdout = orig_stdout
+
+
+
+if __name__ == '__main__':
+    unittest.main()
