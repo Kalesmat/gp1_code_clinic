dfd7ecb3f3f8689776f9d3ac3d0a5046c6ed76f3
Luke Templeman <72209314+LukeTempleman@users.noreply.github.com>
2020-11-12 13:44:01 +0200

    Commented out Google API section for iteration 1

--- patient/patient_view_open_booking.py
+++ patient/patient_view_open_booking.py
@@ -1,6 +1,6 @@
-import datetime
+# import datetime
 
-def view_open_bookings(service):
+def view_open_bookings():
     """
     The patient will be able to see a list of all available slots
     """
@@ -11,19 +11,19 @@ def view_open_bookings(service):
     """
     print(avail_slots)
 
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='codeclinic.team14@gmail.com', timeMin=now,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
+#     # Call the Calendar API
+#     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+#     print('Getting the upcoming 10 events')
+#     events_result = service.events().list(calendarId='codeclinic.team14@gmail.com', timeMin=now,
+#                                         maxResults=10, singleEvents=True,
+#                                         orderBy='startTime').execute()
+#     events = events_result.get('items', [])
 
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+#     if not events:
+#         print('No upcoming events found.')
+#     for event in events:
+#         start = event['start'].get('dateTime', event['start'].get('date'))
+#         print(start, event['summary'])
 
 
-    return avail_slots
+#     return avail_slots
