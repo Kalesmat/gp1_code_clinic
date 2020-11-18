d39824d9fe4ed305e60ffb78571625ec928ca8f6
moolivieJHB <67946957+moolivieJHB@users.noreply.github.com>
2020-11-18 17:49:40 +0200

    Update view_events.py
    Iteration 2

--- clinician/view_events.py
+++ clinician/view_events.py
@@ -28,7 +28,6 @@ def view(service):
     #iteration 1
     events_set=['Recursion ' ,' Luke','11/11/2020' ,'14:30']
     for i in range(len(events_set)):
-
         print(str(events_set[i]))
     return str(events_set)
 
