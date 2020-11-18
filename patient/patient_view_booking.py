773ab37b6e3c4f9f8773cd51b8de90e882df4170
{ltemplem} <{ltemplem@student.wethinkcode.co.za}>
2020-11-18 15:54:22 +0200

    Added the view booked events function

--- patient/patient_view_booking.py
+++ patient/patient_view_booking.py
@@ -1,9 +1,40 @@
 def view_booking(service):
-    """"
-    Patient will be able to see a list of all their bookings.
-    """
-    my_appointments = """These are your bookings:
-        15h00 - Booking with BoJack for Toy Robot 4."""
-    print(my_appointments)
+    '''
+    Patient will be able to view all their bookings
+    PARAMS : the service instance
+    '''
 
-    return my_appointments
+    print("These Are Your Booked Events:")
+    page_token = None
+    while True:
+        events = service.events().list(calendarId='primary', pageToken=page_token).execute()
+        
+        for event in events['items']:
+            try:
+            # Dictionary Unpacking with variables
+                summary = event['summary']
+                
+                event_creator = event['creator']
+                creator = event_creator['email']
+                id_user = event['id']
+
+                #Unpacking Time Dictionaries
+                event_time_start = event['start']
+                event_time_end = event['end']
+                start = event_time_start['dateTime']
+                end = event_time_end['dateTime']
+                
+                #Output of the Data
+                if event['status'] == "confirmed":
+                    print("----------------")
+                    print(f'{summary} by {creator}')
+                    print(f'starts at {start} and ends at {end}')
+                    print(f'Id is: ({id_user})')
+
+            except KeyError:
+                break
+
+            page_token = events.get('nextPageToken')
+        if not page_token:
+            break
+        
