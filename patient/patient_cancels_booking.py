5d9677d1fedbab0046c0d263042d2bdb2462fe17
FWeni <73465560+FWeni@users.noreply.github.com>
2020-11-19 14:54:45 +0200

    Update patient_cancels_booking.py
    added functionality to patient_cancels_booking

--- patient/patient_cancels_booking.py
+++ patient/patient_cancels_booking.py
@@ -1,7 +1,26 @@
+from patient import patient_view_booking
+from pprint import  pprint
+
 def cancel_booking(service):
     """
     Patient cancels booking after giving a reason for cancellation.
     """
     text = "Booking cancelled."
     print(text)
+
+    events = patient_view_booking.view_booking(service)
+    pprint(events)
+    eventid = input("Please insert the event ID: ")
+    event = service.events().get(calendarId='primary', eventId=eventid).execute()
+
+    event['status'] = 'confirmed'
+    admin = event['attendees'][0]['email']
+    event['attendees'] = [
+        {'email': admin},
+
+    ]
+
+    updated_event = service.events().update(calendarId='primary', eventId=eventid, body=event, ).execute()
+    pprint(updated_event['updated'])
+    pprint("Event Cancelled")
     return text
