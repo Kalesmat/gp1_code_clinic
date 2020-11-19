9da3f71f3cf00904ce1b3906d538362268f73552
IKalonji <IKalonji@student.wethinkcode.co.za>
2020-11-19 10:09:09 +0200

    Added API functionality to the patient view available slots module

--- patient/patient_view_open_booking.py
+++ patient/patient_view_open_booking.py
@@ -4,25 +4,21 @@ from datetime import timedelta
 def view_open_bookings(service):
     '''Function to get the next 7 days events'''
     # Call the Calendar API
+ 
     now = datetime.datetime.utcnow()
     end_time = now + timedelta(days=7)
     end_time = end_time.isoformat() + 'Z' # 'Z' indicates UTC time
     now = now.isoformat() + 'Z' # 'Z' indicates UTC time
-    
-    print('Getting the upcoming events for the next 7 days')
-    events_result = service.events().list(calendarId='codeclinic.team14@gmail.com', timeMin=now,
-                                        timeMax=end_time, singleEvents=True,
-                                        orderBy='startTime').execute()
+    events_result = service.events().list(
+       calendarId='codeclinic.team14@gmail.com', timeMin=now,
+       timeMax=end_time, singleEvents=True,maxAttendees=1,
+       orderBy='startTime').execute()
     events = events_result.get('items', [])
 
     if not events:
-        print('No upcoming events found.')
+       print('No upcoming events found.')
     for event in events:
-        if len(events['attendees']) > 2:
-            pass
-        else:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'], event['eventID'])
+       start = event['start'].get('dateTime') #, event['start'].get('date')
+       print(start.strip(), event['summary'], event['id'], event['creator'])
 
-
-    # return avail_slots
+  
