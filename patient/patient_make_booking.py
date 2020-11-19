34dbb61a0766a6681e8f97db79ad62ab1328f97a
FWeni <73465560+FWeni@users.noreply.github.com>
2020-11-19 14:55:48 +0200

    Update patient_make_booking.py
    Added some functionality to make_booking

--- patient/patient_make_booking.py
+++ patient/patient_make_booking.py
@@ -1,13 +1,26 @@
+from pprint import pprint
+from patient import patient_view_open_booking
+
 
 def booking(service):
     """
-    Adds a booking to the google calendar
+     Adds a booking to the google calendar
     """
+    events = patient_view_open_booking.view_open_bookings(service)
+    pprint(events)
+    eventid = input("Please insert the event ID: ")
+    event = service.events().get(calendarId='primary', eventId=eventid).execute()
 
-    text = "You have just made an appointment."
-    print(text)
+    event['status'] = 'confirmed'
+    admin = event['attendees'][0]['email']
+    event['attendees'] = [
+        {'email': admin},
+        {'email': 'patient.cc.team14@gmail.com'},
+    ]
 
-    return text
+    updated_event = service.events().update(calendarId='primary', eventId=eventid,body=event, ).execute()
+    pprint(updated_event['updated'])
+
 
 
 
