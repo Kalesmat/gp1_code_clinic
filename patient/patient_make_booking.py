a7b29735abfc8e1ee2871782d5b3bf1ed537f524
FWeni <73465560+FWeni@users.noreply.github.com>
2020-11-20 10:41:19 +0200

    Update patient_make_booking.py
    Added error handling

--- patient/patient_make_booking.py
+++ patient/patient_make_booking.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+from googleapiclient.errors import HttpError
 from patient import patient_view_open_booking
 
 
@@ -6,21 +7,23 @@ def booking(service):
     """
      Adds a booking to the google calendar
     """
-    events = patient_view_open_booking.view_open_bookings(service)
-    pprint(events)
-    eventid = input("Please insert the event ID: ")
-    event = service.events().get(calendarId='primary', eventId=eventid).execute()
+    try:
+        patient_view_open_booking.view_open_bookings(service)
+        eventid = input("Please insert the event ID: ")
+        event = service.events().get(calendarId='primary', eventId=eventid).execute()
 
-    event['status'] = 'confirmed'
-    admin = event['attendees'][0]['email']
-    event['attendees'] = [
-        {'email': admin},
-        {'email': 'patient.cc.team14@gmail.com'},
-    ]
+        event['status'] = 'confirmed'
+        admin = event['attendees'][0]['email']
+        event['attendees'] = [
+            {'email': admin},
+            {'email': 'patient.cc.team14@gmail.com'},
+        ]
 
-    updated_event = service.events().update(calendarId='primary', eventId=eventid,body=event, ).execute()
-    pprint(updated_event['updated'])
+        updated_event = service.events().update(calendarId='primary', eventId=eventid,body=event, ).execute()
+        pprint(updated_event['updated'])
 
+    except HttpError:
+        pprint("Invalid event ID..")
 
 
 
