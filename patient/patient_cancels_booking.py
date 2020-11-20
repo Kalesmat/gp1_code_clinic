2e91a3bec371972bd4358c55987824ddc0109562
fweni <fweni@student.wethinkcode.co.za>
2020-11-20 10:37:10 +0200

    Included error handling on the booking function and the cancel booking function.

--- patient/patient_cancels_booking.py
+++ patient/patient_cancels_booking.py
@@ -1,7 +1,28 @@
+from pprint import pprint
+from googleapiclient.errors import HttpError
+from patient import patient_view_booking
+
+
 def cancel_booking(service):
     """
     Patient cancels booking after giving a reason for cancellation.
     """
-    text = "Booking cancelled."
-    print(text)
-    return text
+
+    try:
+        patient_view_booking.view_booking(service)
+        eventid = input("Please insert the event ID: ")
+        event = service.events().get(calendarId='primary', eventId=eventid).execute()
+
+        event['status'] = 'confirmed'
+        admin = event['attendees'][0]['email']
+        event['attendees'] = [
+            {'email': admin},
+
+        ]
+
+        updated_event = service.events().update(calendarId='primary', eventId=eventid, body=event, ).execute()
+        pprint(updated_event['updated'])
+        pprint("Event Cancelled")
+
+    except HttpError:
+        pprint("Invalid event ID..")
--- patient/patient_make_booking.py
+++ patient/patient_make_booking.py
@@ -1,13 +1,28 @@
+from pprint import pprint
+from googleapiclient.errors import HttpError
+from patient import patient_view_open_booking
+
 
 def booking(service):
     """
-    Adds a booking to the google calendar
+     Adds a booking to the google calendar
     """
+    try:
+        patient_view_open_booking.view_open_bookings(service)
+        eventid = input("Please insert the event ID: ")
+        event = service.events().get(calendarId='primary', eventId=eventid).execute()
 
-    text = "You have just made an appointment."
-    print(text)
+        event['status'] = 'confirmed'
+        admin = event['attendees'][0]['email']
+        event['attendees'] = [
+            {'email': admin},
+            {'email': 'patient.cc.team14@gmail.com'},
+        ]
 
-    return text
+        updated_event = service.events().update(calendarId='primary', eventId=eventid,body=event, ).execute()
+        pprint(updated_event['updated'])
 
+    except HttpError:
+        pprint("Invalid event ID..")
 
 
