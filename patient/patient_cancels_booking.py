3bb456620a2212f8712abc28c8be8ca2394409cc
FWeni <73465560+FWeni@users.noreply.github.com>
2020-11-25 14:35:42 +0200

    Update patient_cancels_booking.py
    update the cancel function to check if the person who is cancelling was an attendee in the event

--- patient/patient_cancels_booking.py
+++ patient/patient_cancels_booking.py
@@ -3,7 +3,7 @@ from googleapiclient.errors import HttpError
 from patient import patient_view_booking
 
 
-def cancel_booking(service):
+def cancel_booking(service, email):
     """
     Patient cancels booking after giving a reason for cancellation.
     """
@@ -11,7 +11,7 @@ def cancel_booking(service):
     try:
         my_events = patient_view_booking.view_booking(service)
         if my_events is None:
-            pprint("There are no bookings made.")
+            pprint("There are no recent bookings made.")
             return 0
         else:
             eventid = input("Please insert the event ID: ")
@@ -19,13 +19,22 @@ def cancel_booking(service):
 
             event['status'] = 'confirmed'
             admin = event['attendees'][0]['email']
-            event['attendees'] = [
-                {'email': admin},
-            ]
+            email2 = event['attendees'][1]['email']
 
-            updated_event = service.events().update(calendarId='primary', eventId=eventid, body=event, ).execute()
-            pprint(updated_event['updated'])
-            pprint("Event Cancelled.")
+            if email == email2:
+                event['attendees'] = [
+                    {'email': admin},
+                ]
+                updated_event = service.events().update(calendarId='primary', eventId=eventid, body=event, ).execute()
+                pprint(updated_event['updated'])
+                pprint("You have successfully cancelled slot.")
+            else:
+                pprint("You are not the attendee on this event.")
 
     except HttpError:
-        return"Invalid event ID.."
+        print("Unfortunately that is an invalid event ID..")
+        return False
+
+    except IndexError:
+        print("No recent bookings matching the event ID were found.")
+        return False
