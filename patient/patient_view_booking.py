4528886393b02efbafa8dd4baf7007f800488dfc
Luke Templeman <72209314+LukeTempleman@users.noreply.github.com>
2020-11-25 15:05:37 +0200

    fixed to view proper bookings by checking amount of attendees

--- patient/patient_view_booking.py
+++ patient/patient_view_booking.py
@@ -1,40 +1,47 @@
-def view_booking(service):
+def view_booking(service, email):
     '''
     Patient will be able to view all their bookings
     PARAMS : the service instance
     '''
 
-    print("These Are Your Booked Events:")
     page_token = None
     while True:
         events = service.events().list(calendarId='primary', pageToken=page_token).execute()
-        
-        for event in events['items']:
-            try:
-            # Dictionary Unpacking with variables
-                summary = event['summary']
-                
-                event_creator = event['creator']
-                creator = event_creator['email']
-                id_user = event['id']
 
-                #Unpacking Time Dictionaries
-                event_time_start = event['start']
-                event_time_end = event['end']
-                start = event_time_start['dateTime']
-                end = event_time_end['dateTime']
-                
-                #Output of the Data
-                if event['status'] == "confirmed":
-                    print("----------------")
-                    print(f'{summary} by {creator}')
-                    print(f'starts at {start} and ends at {end}')
-                    print(f'Id is: ({id_user})')
+        if not events["summary"]:
+            print("You have no events booked.")
 
-            except KeyError:
-                break
+        else:
+            print("These Are Your Booked Events:")
 
-            page_token = events.get('nextPageToken')
+            for event in events['items']:
+                try:
+                # Dictionary Unpacking with variables
+                    summary = event['summary']
+
+                    event_creator = event['creator']
+                    creator = event_creator['email']
+                    id_user = event['id']
+
+                    #Unpacking Time Dictionaries
+                    event_time_start = event['start']
+                    event_time_end = event['end']
+                    start = event_time_start['dateTime']
+                    end = event_time_end['dateTime']
+
+                    #Output of the Date
+                    if len(event['attendees']) == 2:
+                        patient_email = event['attendees'][1]["email"]
+                        if patient_email == email:
+                            print("----------------")
+                            print(f'{summary} by {creator}')
+                            print(f'starts at {start} and ends at {end}')
+                            print(f'Id is: {id_user}')
+
+                except KeyError:
+                    break
+
+                page_token = events.get('nextPageToken')
         if not page_token:
             break
         
