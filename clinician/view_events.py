e9525693f0be3d3c539c9ab73628b9af0ecd4eb0
moolivieJHB <67946957+moolivieJHB@users.noreply.github.com>
2020-11-25 14:09:48 +0200

    Update view_events.py
    printing your own events

--- clinician/view_events.py
+++ clinician/view_events.py
@@ -1,9 +1,10 @@
 import datetime
 
-def view(service):
+
+def view(service,myemail):
    
     '''This function displays the events that have been booked.'''
-
+    
     page_token = None
     now = datetime.datetime.now().isoformat() + 'Z'
     n = 0
@@ -11,19 +12,33 @@ def view(service):
         events = service.events().list(calendarId='primary', timeMin=now, pageToken=page_token).execute()
         for event in events['items']:
             try:
-                summary =event['summary']
                 
-                start=event['start']
-                end= event['end']
+
+                summary =event['summary']
+                event_creator = event['creator']
+                creator = event_creator['email']
+                
                 status= event['status']
                 id= event['id']
 
-                n+=1
-                print(f'You have a {summary} session event Id {id} at {start} till {end} ')
-                print(f'Status: {status}')
-
+                #unpack time
+                event_time_start = event['start']
+                event_time_end = event['end']
+                start = event_time_start['dateTime']
+                end = event_time_end['dateTime']
+                
+            
+                
+                admin = event['attendees'][0]['email']
+                if myemail == admin:
+                    print("----------------")
+                    print(f'{summary} created by {admin}')
+                    print(f'starts at {start} and ends at {end}')
+                    print(f'Id is: {id}')
+                    n += 1
+                
             except KeyError:
-        
+              
                 break
         if n < 1:
             print('You have not volunteered')
