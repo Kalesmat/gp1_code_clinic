de82eda96de9444a59bd9e567ae9523aa9a27f62
moolivieJHB <67946957+moolivieJHB@users.noreply.github.com>
2020-11-27 09:52:22 +0200

    Update view_events.py
    update return statement

--- clinician/view_events.py
+++ clinician/view_events.py
@@ -1,48 +1,52 @@
 import datetime
 
 
-def view(service,myemail):
-   
+def view(service, myemail):
     '''This function displays the events that have been booked.'''
-    
+
+    message = ''
     page_token = None
     now = datetime.datetime.now().isoformat() + 'Z'
     n = 0
     while True:
-        events = service.events().list(calendarId='primary', timeMin=now, pageToken=page_token).execute()
+
+        events = service.events().list(calendarId='primary', timeMin=now,
+                                       pageToken=page_token).execute()
+
         for event in events['items']:
             try:
-                
 
-                summary =event['summary']
+                summary = event['summary']
                 event_creator = event['creator']
                 creator = event_creator['email']
-                
-                status= event['status']
-                id= event['id']
+
+                status = event['status']
+                id = event['id']
 
                 #unpack time
                 event_time_start = event['start']
                 event_time_end = event['end']
                 start = event_time_start['dateTime']
                 end = event_time_end['dateTime']
-                
-            
-                
+
                 admin = event['attendees'][0]['email']
                 if myemail == admin:
-                    print("----------------")
-                    print(f'{summary} created by {admin}')
-                    print(f'starts at {start} and ends at {end}')
-                    print(f'Id is: {id}')
+                    message = (
+                        f"----------------\n{summary} created by {admin}\nstarts at {start} and ends at {end}\nId is: {id}")
+
+                    print(message)
                     n += 1
-                
             except KeyError:
-              
+
                 break
         if n < 1:
-            print('You have not volunteered')
+            message = 'You have not volunteered'
+            print(message)
+        else:
+            message = "You have Volunteered"
         page_token = events.get('nextPageToken')
         if not page_token:
             break
+
+    return message
 
