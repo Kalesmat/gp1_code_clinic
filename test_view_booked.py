a84131cc9f6771229ae44873cb5d8206f8cbc356
Luke Templeman <72209314+LukeTempleman@users.noreply.github.com>
2020-12-02 13:22:41 +0200

    Added a unit test for no booked events
    have one more to do

--- test_view_booked.py
+++ test_view_booked.py
@@ -1,26 +1,18 @@
 import unittest
-from clinician import create
-from io import StringIO
-import sys
+import patient.patient_view_booking as patient
 from code_clinic import startup
-import os
-import sys
-from unittest.mock import patch
-import patient.patient_view_booking
 
 service = startup()
 class test_booked(unittest.TestCase):
     
-    def test_booked(self):
-        self.assertEqual(patient.patient_view_booking.view_booking(service,"ltemplem@student.wethinkcode.co.za"),"""----------------
-Code Clinic: yeetus by jmotebej@student.wethinkcode.co.za
-starts at 2020-11-28T12:00:00+02:00 and ends at 2020-11-28T12:30:00+02:00
-Id is: 618hhpv0bro56h0o01bf0eubsc """)
+    def test_no_slots_booked(self):
+        self.assertEqual(patient.view_booking(service,"ltemplem@student.wethinkcode.co.za"), "You have no booked slots")
 
-    def test_wrongbooked(self):
-        self.assertNotEqual(patient.patient_view_booking.view_booking(service,"ltemplem@student.wethinkcode.co.za"),"""----------------
-Code Clinic: yeetus by jmotebej@student.wethinkcode.co.za
-starts at 2020-11-28T12:00:00+02:00 and ends at 2020-11-28T12:30:00+02:
-Id is: 618hhpv0bro56h0o01bf0eubsc """)
+
+
+    #need to add 1 more unit test 
+    # def test_booked_slot(self):
+
+
 if __name__ == "__main__":
     unittest.main()
