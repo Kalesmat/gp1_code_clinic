2c745ab8d7dd786c5448fa66192519b0898b969b
Luke Templeman <72209314+LukeTempleman@users.noreply.github.com>
2020-12-02 16:45:15 +0200

    finished Unit Testing

--- test_view_booked.py
+++ test_view_booked.py
@@ -1,17 +1,32 @@
 import unittest
 import patient.patient_view_booking as patient
 from code_clinic import startup
+# import 
 
 service = startup()
 class test_booked(unittest.TestCase):
-    
+
+
     def test_no_slots_booked(self):
         self.assertEqual(patient.view_booking(service,"ltemplem@student.wethinkcode.co.za"), "You have no booked slots")
+   
+
+    def test_1_booked_slot(self):
+        patient.n = 1
+        self.assertEqual(patient.view_booking(service,"ltemplem@student.wethinkcode.co.za"), "\nYou have 1 booked slot")
+        patient.n = 0
 
 
+    def test_2_booked_slots(self):
+        patient.n = 2
+        self.assertEqual(patient.view_booking(service,"ltemplem@student.wethinkcode.co.za"), "\nYou have 2 booked slots")
+        patient.n = 0 
 
-    #need to add 1 more unit test 
-    # def test_booked_slot(self):
+    
+    def test_20_booked_slots(self):
+        patient.n = 20
+        self.assertEqual(patient.view_booking(service,"ltemplem@student.wethinkcode.co.za"), "\nYou have 20 booked slots")
+        patient.n = 0 
 
 
 if __name__ == "__main__":
