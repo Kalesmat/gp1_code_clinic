85abebaa404f3d880eed794224c40074952abdb3
Luke Templeman <72209314+LukeTempleman@users.noreply.github.com>
2020-12-04 10:27:22 +0200

    Fixed STD_OUT so no output is shown

--- test_view_booked.py
+++ test_view_booked.py
@@ -1,33 +1,46 @@
+import io
+import sys
 import unittest
 import patient.patient_view_booking as patient
 from code_clinic import startup
-# import 
+
 
 service = startup()
 class test_booked(unittest.TestCase):
 
 
     def test_no_slots_booked(self):
-        self.assertEqual(patient.view_booking(service,"ltemplem@student.wethinkcode.co.za"), "You have no booked slots")
+        suppress_text = io.StringIO()
+        sys.stdout = suppress_text 
    
+        self.assertEqual(patient.view_booking(service,"ltemplem@student.wethinkcode.co.za"), "You have no booked slots")
+        sys.stdout = sys.__stdout__
+
 
     def test_1_booked_slot(self):
+        suppress_text = io.StringIO()
+        sys.stdout = suppress_text 
         patient.n = 1
         self.assertEqual(patient.view_booking(service,"ltemplem@student.wethinkcode.co.za"), "\nYou have 1 booked slot")
         patient.n = 0
-
+        sys.stdout = sys.__stdout__
 
     def test_2_booked_slots(self):
+        suppress_text = io.StringIO()
+        sys.stdout = suppress_text 
         patient.n = 2
         self.assertEqual(patient.view_booking(service,"ltemplem@student.wethinkcode.co.za"), "\nYou have 2 booked slots")
         patient.n = 0 
-
+        sys.stdout = sys.__stdout__
     
     def test_20_booked_slots(self):
+        suppress_text = io.StringIO()
+        sys.stdout = suppress_text 
         patient.n = 20
         self.assertEqual(patient.view_booking(service,"ltemplem@student.wethinkcode.co.za"), "\nYou have 20 booked slots")
         patient.n = 0 
-
+        sys.stdout = sys.__stdout__
 
 if __name__ == "__main__":
     unittest.main()
+
