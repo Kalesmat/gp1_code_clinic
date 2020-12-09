0b2c1e776c14cd3b94e92d4e251d77998064d1a9
jmotebej <73529547+jmotebej@users.noreply.github.com>
2020-12-09 11:44:09 +0200

    Update test_clinician_create.py
    Updated with doc strings

--- test_clinician_create.py
+++ test_clinician_create.py
@@ -12,9 +12,12 @@ import datetime
 
 class TestCase(unittest.TestCase):
 
-    def test_create_wrong_date_n(self):
+    def test_create_wrong_date(self):
+        """
+        Test Create function if the date is older than now
+        """
         service = code_clinic.startup()
-        username, email = code_clinic.get_credentials()
+        username, email, name = code_clinic.get_credentials()
 
         Summary = 'Test_create'
         Descript = 'Test function create'
@@ -22,19 +25,22 @@ class TestCase(unittest.TestCase):
         startT = '12:30'        
         
         TestD="01/01/01"
-        with patch('sys.stdin', StringIO(f'{TestD}\nn\n')):
+        with patch('sys.stdin', StringIO(f'{TestD}\n')):
             orig_stdout = sys.stdout
             new_string = StringIO()
             sys.stdout = new_string
             create.create(service,username, email)
             output = sys.stdout.getvalue().strip()
-            self.assertTrue(f"Bye {username}" in output)
+            self.assertTrue(f"Bye {username}",output)
             sys.stdout = orig_stdout
 
         
     def test_create_y(self):
+        """
+        Test Create function for confirming the creation of an event for y
+        """
         service = code_clinic.startup()
-        username, email = code_clinic.get_credentials()
+        username, email, name = code_clinic.get_credentials()
 
         Summary = 'Test_create'
         Descript = 'Test function create'
@@ -48,22 +54,23 @@ class TestCase(unittest.TestCase):
             sys.stdout = new_string
             event_id = create.create(service,username, email)
             output = sys.stdout.getvalue().strip()
-            self.assertTrue("Event Created" in output)
+            self.assertTrue("Event Created",output)
             delete.do_delete(service, email, event_id)
             sys.stdout = orig_stdout
 
 
     def test_create_n(self):
+        """
+        Test Create function for confirming the creation of an event for n
+        """
         service = code_clinic.startup()
-        username, email = code_clinic.get_credentials()
+        username, email, name= code_clinic.get_credentials()
 
         Summary = 'Test_create'
         Descript = 'Test function create'
         startD = datetime.datetime.now().date() + datetime.timedelta(days=5)
         startT = '12:30'
-        endT = '13:00'
-        # event = create.do_create(service,Summary,Description,startD,startT,endT,username,email)
-        # event_id = event['id']
+        endT = '13:00'        
         
         TestD=str(startD.day)+"/"+str(startD.month)+"/"+str(startD.year)
         with patch('sys.stdin', StringIO(f'{TestD}\n{startT}\n{Summary}\n{Descript}\nn\n')):
@@ -72,33 +79,11 @@ class TestCase(unittest.TestCase):
             sys.stdout = new_string
             event_id = create.create(service,username, email)
             output = sys.stdout.getvalue().strip()
-            self.assertTrue("you have not created the event" in output)
+            self.assertTrue("you have not created the event",output)
             delete.do_delete(service, email, event_id)
             sys.stdout = orig_stdout
 
-    # def test_do_create(self):
-    #     """
-    #     Testing the return of create.do_create()
-    #     """
-        
-    #     # orig_stdout = sys.stdout
-    #     # new_string = StringIO()
-    #     # sys.stdout = new_string
-
-    #     user, email = code_clinic.get_credentials()
-    #     day = datetime.datetime.now().date() + datetime.timedelta(days=4)
-    #     # day = day.isoformat() + 'Z'
-    #     print(day)
-    #     sum = "Testing do_Create"
-    #     des = "Using datetime"
-
-
-    #     ser = code_clinic.startup()
-    #     output=create.do_create(ser,sum,des,day,"12:00","12:30",user,email)
-    #     print(output)
-        
     
-
 
 if __name__ == "__main__":
     unittest.main()
