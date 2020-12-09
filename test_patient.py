31c53edc905c99d1298ec4ab2abd39534759ea76
FWeni <73465560+FWeni@users.noreply.github.com>
2020-12-09 15:01:39 +0200

    Update test_patient.py
    hid open_view and view booking output that was showing when I was running my tests

--- test_patient.py
+++ test_patient.py
@@ -21,7 +21,6 @@ service = code_clinic.startup()
 creator, admin = "Fake Creator", "fake.creator@gmail.com"
 create_slot = create.do_create(service, summary, description, start_d, start_t, end_t, creator, admin)
 test_id = create_slot['id']
-my_events = patient_view_booking.view_booking(service, admin)
 
 
 class PatientTest(unittest.TestCase):
@@ -33,7 +32,6 @@ class PatientTest(unittest.TestCase):
         book = patient_make_booking.booking(service, username, email, test_id)
         test_result = f"{summary} is successfully booked.."
         self.assertTrue(book, test_result)
-        self.assertTrue(test_id, my_events is True)
         delete.do_delete(service, admin, test_id)
         sys.stdout = the_stdout
 
@@ -45,6 +43,7 @@ class PatientTest(unittest.TestCase):
         book = patient_make_booking.booking(service, username, email, test_id)
         test_result = f"{username}, number of attendees has been reached, please check for the next slot."
         self.assertTrue(book, test_result)
+        delete.do_delete(service, admin, test_id)
         sys.stdout = the_stdout
 
     def test_patient_make_booking_if_is_volunteer(self):
@@ -55,6 +54,7 @@ class PatientTest(unittest.TestCase):
         book = patient_make_booking.booking(service, username, email, test_id)
         test_result = f'{username}, Unfortunately you cannot book your own event..'
         self.assertTrue(book, test_result)
+        delete.do_delete(service, admin, test_id)
         sys.stdout = the_stdout
 
     def test_patient_make_booking_invalid_eventid(self):
@@ -65,6 +65,7 @@ class PatientTest(unittest.TestCase):
         book = patient_make_booking.booking(service, username, email, "y0hI@mFak3eee33")
         test_result = "Unfortunately that is an invalid event ID.."
         self.assertFalse(book, test_result)
+        delete.do_delete(service, admin, test_id)
         sys.stdout = the_stdout
 
     def test_patient_make_booking_empty(self):
@@ -85,7 +86,7 @@ class PatientTest(unittest.TestCase):
         booked = patient_make_booking.booked(service, email, test_id)
         test_result = f"Failed to book because:\n- You will be consulted by {admin} on {summary}" \
                       f"\n- From {start_t} until {end_t}"
-        self.assertFalse(booked, test_result)
+        self.assertTrue(booked, test_result)
         delete.do_delete(service, admin, test_id)
         sys.stdout = the_stdout
 
@@ -98,7 +99,7 @@ class PatientTest(unittest.TestCase):
         booked = patient_make_booking.booked(service, email, test_id)
         test_result = f"Failed to book because:\n- You are a clinician on {summary}" \
                       f"\n- From {start_t} until {end_t}"
-        self.assertFalse(booked, test_result)
+        self.assertTrue(booked, test_result)
         self.assertTrue(created, booked)
         delete.do_delete(service, admin, test_id)
         sys.stdout = the_stdout
@@ -111,7 +112,7 @@ class PatientTest(unittest.TestCase):
         cancel = patient_cancels_booking.cancel_booking(service, username, email, test_id)
         test_result = f"{username}, You have successfully cancelled your booking."
         self.assertTrue(cancel, test_result)
-        self.assertTrue(test_id, my_events is True)
+        delete.do_delete(service, admin, test_id)
         sys.stdout = the_stdout
 
     def test_patient_cancels_booking_my_events_is_none(self):
@@ -119,20 +120,23 @@ class PatientTest(unittest.TestCase):
         new_string = StringIO()
         sys.stdout = new_string
         username, email = "Booker", "fake.booking@gmail.com"
+        my_events = patient_view_booking.view_booking(service, admin)
         cancel = patient_cancels_booking.cancel_booking(service, username, email, my_events)
         self.assertFalse(cancel, my_events is False)
+        delete.do_delete(service, admin, test_id)
         sys.stdout = the_stdout
 
     def test_patient_cancels_booking_if_event_is_other_patients(self):
-        events = patient_view_open_booking.view_open_bookings(service)
         the_stdout = sys.stdout
         new_string = StringIO()
         sys.stdout = new_string
         username, email = "Booker", "fake.booking@gmail.com"
+        events = patient_view_open_booking.view_open_bookings(service)
         cancel = patient_cancels_booking.cancel_booking(service, username, email, events)
         test_result = f"{username}, You are not the attendee on this event."
         self.assertFalse(cancel, test_result)
         self.assertFalse(events is False)
+        delete.do_delete(service, admin, test_id)
         sys.stdout = the_stdout
 
     def test_patient_cancels_booking_invalid_eventid(self):
@@ -143,6 +147,7 @@ class PatientTest(unittest.TestCase):
         cancel = patient_cancels_booking.cancel_booking(service, username, email, "y0hI@mFak3eee33")
         test_result = "Unfortunately that is an invalid event ID.."
         self.assertFalse(cancel, test_result)
+        delete.do_delete(service, admin, test_id)
         sys.stdout = the_stdout
 
     def test_patient_cancels_booking_invalid_empty(self):
@@ -153,6 +158,7 @@ class PatientTest(unittest.TestCase):
         cancel = patient_cancels_booking.cancel_booking(service, username, email, 'None')
         test_result = "Unfortunately that is an invalid event ID.."
         self.assertFalse(cancel, test_result)
+        delete.do_delete(service, admin, test_id)
         sys.stdout = the_stdout
 
 
