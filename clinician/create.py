06f2880812393db1967f4f1688a6b278460be0df
jmotebej <73529547+jmotebej@users.noreply.github.com>
2020-12-10 13:04:53 +0200

    Update create.py
    Cleaned and ready to be deployed

--- clinician/create.py
+++ clinician/create.py
@@ -6,38 +6,38 @@ from pprint import pprint
 
 def create(service, user, email):
     """
-    Get the Day and Time
+    Get the day and time
     """
-    Day,Year,Month,hour,Min = 0,0,0,0,0
+    day,year,month,hour,minutes = 0,0,0,0,0
 
-    while Day < 1 or Day > 31 or Month < 1 or Month > 12 or Year < 1:
-        Datei = input("Enter Date (Day/Month/Year): ") .strip()
-        if "/" in Datei:
-            Date = Datei.split('/') 
-            if len(Date) != 3 or not Date[0].isdigit() or not Date[1].isdigit()\
-                or not Date[2].isdigit():
-                print("Date should be in this format Day/Month/Year")
+    while day < 1 or day > 31 or month < 1 or month > 12 or year < 1:
+        dateinput = input("Enter date (day/month/year): ") .strip()
+        if "/" in dateinput:
+            date = dateinput.split('/') 
+            if len(date) != 3 or not date[0].isdigit()\
+                 or not date[1].isdigit() or not date[2].isdigit():
+                print("date should be in this format day/month/year")
 
                 continue
         
         else:
-            print("Date should be in this format Day/Month/Year")
+            print("date should be in this format day/month/year")
 
             continue
-        Day = int(Date[0])
-        Month = int(Date[1])
-        Year = int(Date[2])
-        if Day < 1 or Day > 31:
-            print("Date is invalid.")
-        if Month < 1 or Month > 12:
-            print("Month is invalid.")
-        if Year < 1:
-            print("Year is invalid.")
+        day = int(date[0])
+        month = int(date[1])
+        year = int(date[2])
+        if day < 1 or day > 31:
+            print("date is invalid.")
+        if month < 1 or month > 12:
+            print("month is invalid.")
+        if year < 1:
+            print("year is invalid.")
 
     """
     Check the date if is passed  and ask
     """
-    my_date = datetime.datetime(Year, Month, Day,23,30)
+    my_date = datetime.datetime(year, month, day,23,30)
     
     if my_date < datetime.datetime.today():
         message2 = "event cannot be created , day has passed."
@@ -48,38 +48,40 @@ def create(service, user, email):
         return message2
         
 
-    while hour<7 or hour>17 or Min<0 or Min>59 or (hour==17 and Min>30):
-        Timei = input("Enter Time (HH:MM): ").strip()
-        if ":" in Timei:
-            Time = Timei.split(":")
-            if len(Time) != 2 or not Time[0].isdigit() or not Time[1].isdigit():
-                print("Time should be in this format HH:MM")
+    while hour<7 or hour>17 or minutes<0 or minutes>59\
+        or (hour==17 and min>30):
+        timeinput = input("Enter time (HH:MM): ").strip()
+        if ":" in timeinput:
+            time = timeinput.split(":")
+            if len(time) != 2 or not time[0].isdigit()\
+                or not time[1].isdigit():
+                print("time should be in this format HH:MM")
 
                 continue
         else:
-            print("Time should be in this format HH:MM")
+            print("time should be in this format HH:MM")
 
             continue
-        hour = int(Time[0])
-        Min = int(Time[1])  
+        hour = int(time[0])
+        minutes = int(time[1])  
 
         if hour < 7 or hour > 17:
             print("Hour should be between 7 and 17")
-        if Min < 0 or Min > 59:
-            print("Minutes should be between 0 and 59")
-        if hour == 17 and Min > 30:
-            print("Minutes should be between 00-30 since we close at 18:00")            
+        if minutes < 0 or minutes > 59:
+            print("minutes should be between 0 and 59")
+        if hour == 17 and minutes > 30:
+            print("minutes should be between 00-30 since we close at 18:00")            
 
     hour2 = hour      
-    min2 = Min+30
+    minutes2 = minutes+30
 
-    if Min >= 30:
-        min2 = 0
+    if minutes >= 30:
+        minutes2 = 0
         hour2 += 1
-        add = Min - 30
-        min2 += add    
+        add = minutes - 30
+        minutes2 += add    
 
-    my_date = datetime.datetime(Year, Month, Day, hour, Min)
+    my_date = datetime.datetime(year, month, day, hour, minutes)
 
     
     if my_date < datetime.datetime.now():
@@ -91,18 +93,18 @@ def create(service, user, email):
         return message2
             
     else:
-        startD = str(Year)+"-"+str(Month)+"-"+str(Day)
-        startT = str(hour)+":"+str(Min)
-        endT = str(hour2)+":"+str(min2)
+        startday = str(year)+"-"+str(month)+"-"+str(day)
+        starttime = str(hour)+":"+str(minutes)
+        endtime = str(hour2)+":"+str(minutes2)
 
         """
         Checking if you have created Event Before
-            - 30 minutes before start Time
+            - 30 minutes before start time
             - During Available Event
-            - Before the End Time
+            - Before the End time
         """
         
-        if createdE(service,email,my_date):
+        if createdevent(service,email,my_date):
             message = "You will be busy during that time"
             
             return message
@@ -112,14 +114,12 @@ def create(service, user, email):
         Creating the event
         """
 
-        Summary,Descript = "",""
-        while Summary =="":
-            Summary = input("Name of your topic: ").strip()
+        summary,description = "",""
+        while summary =="":
+            summary = input("Name of your topic: ").strip()
             
-        while Descript == "":
-            Descript = input("Describe your topic: ").strip()
-
-        
+        while description == "":
+            description = input("Describe your topic: ").strip()        
 
         confirm = ""
         while confirm.lower() != 'y' or confirm.lower() != 'n':
@@ -129,9 +129,11 @@ def create(service, user, email):
                 break
         
         if confirm.lower() == 'y':
-            event=do_create(service,Summary,Descript,startD,startT,endT,user,email) 
-            message = "Event Created"      
-            print('{}\n -Calender Link: {}'.format(message, event.get('htmlLink'))) 
+            event=do_create(service,summary,description,startday,starttime,\
+                endtime,user,email) 
+            message = "Event Created successfully"      
+            print('{}\n - Calender Link: {}'.format(message,\
+                event.get('htmlLink'))) 
             
         else:
             message = "you have not created the event"
@@ -140,20 +142,21 @@ def create(service, user, email):
         return message
 
 
-def do_create(service,Summary,Descript,startD,startT,endT,username,email):
+def do_create(service,summary,description,startday,\
+    starttime,endtime,username,email):
 
     """
     Function to create the event and return event id
     """
     event = {
-        'summary': 'Code Clinic: {}'.format(Summary),
-        'description': '{}.'.format(Descript),
+        'summary': 'Code Clinic: {}'.format(summary),
+        'description': '{}.'.format(description),
         'start': {
-            'dateTime': '{}T{}:00'.format(startD, startT),
+            'dateTime': '{}T{}:00'.format(startday, starttime),
             'timeZone': 'GMT+02',
         },
         'end': {
-            'dateTime': '{}T{}:00'.format(startD,endT),
+            'dateTime': '{}T{}:00'.format(startday,endtime),
             'timeZone': 'GMT+02',
         },
         'recurrence': [
@@ -179,17 +182,18 @@ def do_create(service,Summary,Descript,startD,startT,endT,username,email):
         },
     }
 
-    event = service.events().insert(calendarId='primary', body=event,sendUpdates='all').execute()
+    event = service.events().insert(calendarId='primary', body=event,\
+        sendUpdates='all').execute()
 
     return event
 
 
-def createdE(service, myemail,my_date):
+def createdevent(service, myemail,my_date):
     """
     Checking if you have created Event Before
-        - 30 minutes before start Time
+        - 30 minutes before start time
         - During Available Event
-        - Before the End Time
+        - Before the End time
         - And you are not a patient during that time
         - Return True else return False
     """
@@ -204,7 +208,7 @@ def createdE(service, myemail,my_date):
         for event in events['items']:
             try:                
                 start = event['start'].get('dateTime')                 
-                start = start.split('T')                
+                start = str(start).split('T')                
                 date = start[0]
                 time = start[1].split('+')
                 time = time[0]
@@ -212,17 +216,20 @@ def createdE(service, myemail,my_date):
                 end_t = time + timedelta(minutes=30) 
                 start_c = time + timedelta(minutes=-30)               
                 time, end_t,start_c = str(time), str(end_t), str(start_c)
-                time, end_t,start_c = time.split(" "), end_t.split(" "), start_c.split(" ")
+                time, end_t = time.split(" "), end_t.split(" ") 
+                start_c = start_c.split(" ")
                 time, end_t,start_c = time[1], end_t[1], start_c[1]
 
                 admin = event['attendees'][0]['email']
                 summary = event['summary']
 
-                Dat=date.split('-')
+                dat=date.split('-')
                 Tim=end_t.split(':')
-                tim=datetime.datetime(int(Dat[0]),int(Dat[1]),int(Dat[2]),int(Tim[0]),int(Tim[1]))
+                tim=datetime.datetime(int(dat[0]),int(dat[1]),\
+                    int(dat[2]),int(Tim[0]),int(Tim[1]))
                 Sta=start_c.split(':')
-                Sta=datetime.datetime(int(Dat[0]),int(Dat[1]),int(Dat[2]),int(Sta[0]),int(Sta[1]))
+                Sta=datetime.datetime(int(dat[0]),int(dat[1]),\
+                    int(dat[2]),int(Sta[0]),int(Sta[1]))
 
                 
                 if myemail == admin:
@@ -238,7 +245,8 @@ def createdE(service, myemail,my_date):
                     if myemail == patient_email:                    
                         if (my_date>Sta and my_date<tim):                        
                             print("Failed to Create a slot because:")
-                            print(f" - You will be busy with {admin} on {summary}")
+                            print(f" - You will be busy with {admin}\
+                                 on {summary}")
                             
                             return True
                        
@@ -252,5 +260,3 @@ def createdE(service, myemail,my_date):
             break
            
     return False
-
-
